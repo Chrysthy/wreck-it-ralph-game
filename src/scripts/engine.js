2a714018c7eb00634acc22b43d94529fab2dbf4f
Chrystine Martins <chrystine.martins@outlook.com>
2025-03-14 19:50:33 -0300

    add addListenerHitBox function, add new values

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -10,6 +10,8 @@ const state = {
     values: {
         timerId: null,
         gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
     }
 
 };
@@ -27,6 +29,8 @@ function randomSquare() {
 
     randomSquare.classList.add("enemy");
 
+    state.values.hitPosition = randomSquare.id;
+
 }
 
 function moveEnemy() {
@@ -40,6 +44,15 @@ function addListenerHitBox() {
 
     state.view.squares.forEach((square) => {
 
+        square.addEventListener('mousedown', () => {
+
+            if (square.id === state.values.hitPosition) {
+                
+                state.values.result++
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+            }
+        })
     })
 }
 
